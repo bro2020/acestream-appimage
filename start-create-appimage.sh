1c1812f453e5752678d9e7b9a64e95a4c6e76179
bro2020 <arm121@i.ua>
2021-12-13 00:46:15 +0200

    Fix создание директории build перед запуском контейнера

--- start-create-appimage.sh
+++ start-create-appimage.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 USER=$USER
 HER=$(dirname $(readlink -f "${0}"))
+mkdir -p "${HER}"/build
 docker run -i --name builder-appimage --privileged -v "${HER}"/build:/opt/ debian:9-slim /bin/bash -c 'apt-get update && apt-get install git fuse curl wget dpkg-cross glib-2.0.0 -y && cd opt/ && git clone https://github.com/bro2020/acestream-appimage.git && cd acestream-appimage/ && ./pkg2appimage.appimage recipes/acestream.yml'
 docker rm builder-appimage
 docker rmi debian:9-slim

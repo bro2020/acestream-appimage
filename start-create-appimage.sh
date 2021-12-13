5c942959871b8f421736092307b3111c237c288d
bro2020 <arm121@i.ua>
2021-12-13 03:58:42 +0200

    Добавлена версионность

--- start-create-appimage.sh
+++ start-create-appimage.sh
@@ -2,28 +2,28 @@
 USER=$USER
 HER=$(dirname $(readlink -f "${0}"))
 WD=$(whereis docker)
-APPVERSION=`cat VERSION`
+ACE_VERSION=`cat VERSION`
 COMMAND="apt-get update && \
 apt-get install git fuse curl wget file binutils glib-2.0.0 -y && \
 cd opt/ && \
 git clone https://github.com/bro2020/acestream-appimage.git && \
 cd acestream-appimage/ && \
-APPVERSION=\"$APPVERSION\" ./pkg2appimage.appimage recipes/acestream.yml"
+ACE_VERSION=\"$ACE_VERSION\" ./pkg2appimage.appimage recipes/acestream.yml"
 if [ -z "$WD" ];
 then 
 echo '### Docker not installed! Canceled ###'
 exit 1
 else
 echo ' ### Docker installed, startind make build... ###'
-mkdir -p "${HER}"/Build/$APPVERSION && \
-rm -rf "${HER}"/Build/$APPVERSION/* && \
-docker run -i --name builder-appimage -e APPVERSION=$APPVERSION --privileged -v "${HER}"/Build/$APPVERSION:/opt/ debian:9-slim /bin/bash -c "$COMMAND" && \
+mkdir -p "${HER}"/Build/$ACE_VERSION && \
+rm -rf "${HER}"/Build/$ACE_VERSION/* && \
+docker run -i --name builder-appimage -e ACE_VERSION=$ACE_VERSION --privileged -v "${HER}"/Build/$ACE_VERSION:/opt/ debian:9-slim /bin/bash -c "$COMMAND" && \
 docker rm builder-appimage && \
 docker rmi debian:9-slim && \
-sudo mv "${HER}"/Build/$APPVERSION/acestream-appimage/out/* "${HER}"/Build/$APPVERSION && \
-sudo rm -rf "${HER}"/Build/$APPVERSION/acestream-appimage/ && \
-sudo chown -R $USER:$USER "${HER}"/Build/$APPVERSION/ && \
-echo "### Build Completed! AppImage file in directory Build/\"$APPVERSION\" ###"
-# || \
-#docker rm builder-appimage; docker rmi debian:9-slim; sudo rm -rf "${HER}"/Build/; echo '### Error! Canceled! ###'
+sudo mv "${HER}"/Build/$ACE_VERSION/acestream-appimage/out/* "${HER}"/Build/$ACE_VERSION && \
+sudo rm -rf "${HER}"/Build/$ACE_VERSION/acestream-appimage/ && \
+sudo chown -R $USER:$USER "${HER}"/Build/$ACE_VERSION/ && \
+echo "### Build Completed! AppImage file in directory Build/\"$ACE_VERSION\" ###" && \
+exit 0 || docker rm builder-appimage; \
+docker rmi debian:9-slim; sudo rm -rf "${HER}"/Build/; echo '### Error! Canceled! ###'; exit 1
 fi

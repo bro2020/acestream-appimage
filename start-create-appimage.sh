3938a1fd99ab8abbcd5b8bd32a03ae1cf0ede2be
bro2020 <arm121@i.ua>
2021-12-13 02:00:17 +0200

    Релиз версии 1.0

--- start-create-appimage.sh
+++ start-create-appimage.sh
@@ -1,10 +1,23 @@
 #!/bin/bash
 USER=$USER
 HER=$(dirname $(readlink -f "${0}"))
-mkdir -p "${HER}"/build
-docker run -i --name builder-appimage --privileged -v "${HER}"/build:/opt/ debian:9-slim /bin/bash -c 'apt-get update && apt-get install git fuse curl wget dpkg-cross glib-2.0.0 -y && cd opt/ && git clone https://github.com/bro2020/acestream-appimage.git && cd acestream-appimage/ && ./pkg2appimage.appimage recipes/acestream.yml'
-docker rm builder-appimage
-docker rmi debian:9-slim
-sudo mv "${HER}"/build/acestream-appimage/out/* "${HER}"/build/
-sudo rm -rf "${HER}"/build/acestream-appimage/
-sudo chown -R $USER:$USER "${HER}"/build/
+WD=$(whereis docker)
+if [ -z "$WD" ];
+then 
+echo '### Docker not installed! Canceled ###'
+exit 1
+else
+echo ' ### Docker installed, startind make build... ###'
+mkdir -p "${HER}"/Build && \
+rm -rf "${HER}"/Build/* && \
+docker run -i --name builder-appimage --privileged -v "${HER}"/Build:/opt/ debian:9-slim /bin/bash -c 'apt-get update && \
+apt-get install git fuse curl wget file binutils glib-2.0.0 -y && cd opt/ && \
+git clone https://github.com/bro2020/acestream-appimage.git && cd acestream-appimage/ && \
+./pkg2appimage.appimage recipes/acestream.yml' && \
+docker rm builder-appimage && \
+docker rmi debian:9-slim && \
+sudo mv "${HER}"/Build/acestream-appimage/out/* "${HER}"/Build/ && \
+sudo rm -rf "${HER}"/Build/acestream-appimage/ ; \
+sudo chown -R $USER:$USER "${HER}"/Build/ ; \
+echo '### Build Completed! AppImage file in directory 'Build' ###'
+fi

beea3f7367d6df9d8a870f3f61a116352c9a3601
bro2020 <arm121@i.ua>
2021-12-13 02:39:19 +0200

    Попытка в версионность

--- start-create-appimage.sh
+++ start-create-appimage.sh
@@ -2,22 +2,26 @@
 USER=$USER
 HER=$(dirname $(readlink -f "${0}"))
 WD=$(whereis docker)
+VERSION=`cat VERSION`
+COMMAND="apt-get update && \
+apt-get install git fuse curl wget file binutils glib-2.0.0 -y && \
+cd opt/ && \
+git clone https://github.com/bro2020/acestream-appimage.git && \
+cd acestream-appimage/ && \
+./pkg2appimage.appimage recipes/acestream.yml"
 if [ -z "$WD" ];
 then 
 echo '### Docker not installed! Canceled ###'
 exit 1
 else
 echo ' ### Docker installed, startind make build... ###'
-mkdir -p "${HER}"/Build && \
-rm -rf "${HER}"/Build/* && \
-docker run -i --name builder-appimage --privileged -v "${HER}"/Build:/opt/ debian:9-slim /bin/bash -c 'apt-get update && \
-apt-get install git fuse curl wget file binutils glib-2.0.0 -y && cd opt/ && \
-git clone https://github.com/bro2020/acestream-appimage.git && cd acestream-appimage/ && \
-./pkg2appimage.appimage recipes/acestream.yml' && \
+mkdir -p "${HER}"/Build/"$VERSION" && \
+rm -rf "${HER}"/Build/"$VERSION"/* && \
+docker run -i --name builder-appimage -e VERSION="$VERSION" --privileged -v "${HER}"/Build/"$VERSION":/opt/ debian:9-slim /bin/bash -c "$COMMAND" && \
 docker rm builder-appimage && \
 docker rmi debian:9-slim && \
-sudo mv "${HER}"/Build/acestream-appimage/out/* "${HER}"/Build/ && \
-sudo rm -rf "${HER}"/Build/acestream-appimage/ ; \
-sudo chown -R $USER:$USER "${HER}"/Build/ ; \
-echo '### Build Completed! AppImage file in directory 'Build' ###'
+sudo mv "${HER}"/Build/"$VERSION"/acestream-appimage/out/* "${HER}"/Build/"$VERSION" && \
+sudo rm -rf "${HER}"/Build/"$VERSION"/acestream-appimage/ ; \
+sudo chown -R $USER:$USER "${HER}"/Build/"$VERSION"/ ; \
+echo "### Build Completed! AppImage file in directory \"Build/$VERSION\" ###"
 fi

449ab287ad7c6b9106870a981d69b213e71c83ca
bro2020 <arm121@i.ua>
2021-12-15 05:17:48 +0200

    Fix build dirs

--- start-create-appimage.sh
+++ start-create-appimage.sh
@@ -3,6 +3,7 @@ USER=$USER
 HER=$(dirname $(readlink -f "${0}"))
 WD=$(whereis docker)
 ACE_VERSION=`cat $HER/VERSION`
+BUILD_TIME=$(date +%d_%m_%Y-%H_%M)
 COMMAND="apt-get update && \
 apt-get install git fuse curl wget file binutils glib-2.0.0 -y && \
 cd opt/ && \
@@ -15,16 +16,17 @@ echo '### Docker not installed! Canceled ###'
 exit 1
 else
 echo ' ### Docker installed, startind make build... ###'
-mkdir -p "${HER}"/Build/$ACE_VERSION && \
-rm -rf "${HER}"/Build/$ACE_VERSION/* && \
-docker run -i --name builder-appimage -e ACE_VERSION=$ACE_VERSION -e USER=$USER --privileged -v "${HER}"/Build/$ACE_VERSION:/opt/ debian:9-slim /bin/bash -c "$COMMAND" && \
+mkdir -p "${HER}"/build/${ACE_VERSION}-${BUILD_TIME} && \
+mkdir -p "${HER}"/tmp && \
+rm -rf "${HER}"/tmp/* && \
+docker run -i --name builder-appimage -e ACE_VERSION=$ACE_VERSION -e USER=$USER --privileged -v "${HER}"/tmp:/opt/ debian:9-slim /bin/bash -c "$COMMAND" && \
 docker rm builder-appimage && \
 docker rmi debian:9-slim && \
-sudo mv "${HER}"/Build/$ACE_VERSION/acestream-appimage/out/* "${HER}"/Build/$ACE_VERSION/AceStream-"$ACE_VERSION".AppImage && \
-cp "${HER}"/acestream.conf "${HER}"/Build/$ACE_VERSION/ && \
-sudo rm -rf "${HER}"/Build/$ACE_VERSION/acestream-appimage/ && \
-sudo chown -R $USER:$USER "${HER}"/Build/$ACE_VERSION/ && \
-echo "### Build Completed! AppImage file in directory Build/$ACE_VERSION/AceStream-$ACE_VERSION.AppImage ###" && \
+sudo mv "${HER}"/tmp/acestream-appimage/out/* "${HER}"/build/${ACE_VERSION}-${BUILD_TIME}/AceStream-"$ACE_VERSION".AppImage && \
+cp "${HER}"/acestream.conf "${HER}"/build/${ACE_VERSION}-${BUILD_TIME}/ && \
+sudo rm -rf "${HER}"/tmp && \
+sudo chown -R $USER:$USER "${HER}"/build/${ACE_VERSION}-${BUILD_TIME}/* && \
+echo "### build Completed! AppImage file in directory build/${ACE_VERSION}-${BUILD_TIME}/AceStream-$ACE_VERSION.AppImage ###" && \
 exit 0 || docker rm builder-appimage; \
-docker rmi debian:9-slim; sudo rm -rf "${HER}"/Build/; echo '### Error! Canceled! ###'; exit 1
+docker rmi debian:9-slim; sudo rm -rf "${HER}"/tmp; echo '### Error! Canceled! ###'; exit 1
 fi

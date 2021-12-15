1ac3845bd2a7b18fe03ecd146a33e2c13c3d526b
bro2020 <arm121@i.ua>
2021-12-15 02:36:42 +0200

    Тест автоподтягивания конфига при запуске

--- acestream-server-run.sh
+++ acestream-server-run.sh
@@ -8,7 +8,7 @@ HTTP_PORT=6878
 
 mkdir -p /home/$USER/.ACEStream/
 
-"${HER}"/Build/$ACE_VERSION/AceStream-"$ACE_VERSION".AppImage --client-console --host 127.0.0.1 --port $PORT --http-port $HTTP_PORT --access-token $TOKEN --service-access-token $USER --stats-report-peers --live-buffer 35 --vod-buffer 80 --max-connections 500 --vod-drop-max-age 120 --max-peers 100 --max-upload-slots 20 --download-limit 0 --stats-report-interval 2 --slots-manager-use-cpu-limit 1 --core-dlr-periodic-check-interval 5 --check-live-pos-interval 5 --refill-buffer-interval 1 --core-skip-have-before-playback-pos 1 --webrtc-allow-outgoing-connections 1 --upload-limit 100 --cache-dir /tmp/.ACEStream --state-dir /tmp/.ACEStream --log-file /home/$USER/.ACEStream/acestream.log --log-debug 0 &
+"${HER}"/Build/$ACE_VERSION/AceStream-"$ACE_VERSION".AppImage @$HER/engine.conf &
 
 echo "Сервер доступен локально по адресу: http://localhost:$HTTP_PORT/webui/app/$TOKEN/server#proxy-server-main"
 sleep 3
